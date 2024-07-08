8150e6fccabbaaba420c9755cd351249f0252f3e
Juliana de Oliveira <oliveiramjuliana22@gmail.com>
2024-07-07 23:49:11 -0300

    add new test

---
+++ cypress/e2e/validacao_senha.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit(' http://127.0.0.1:8080')
+    cy.get('#signup-firstname').type('Luana')
+    cy.get('#signup-lastname').type('Vieira')
+    cy.get('#signup-email').type('luana@teste.com')
+    cy.get('#signup-phone').type('1243452465')
+    cy.get('#signup-password').type('teste123')
+    cy.get('#signup-button').click()
+    cy.get('#signup-response').should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*)')
+  })
+})
